7c4120ae29e9a4ed466689843953e9bd617563b5
James Forbes <james.a.forbes@gmail.com>
2021-06-02 09:40:25 +1000

    UMD build

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+import common from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import {terser} from 'rollup-plugin-terser'
+
+export default [
+    {
+        plugins: [
+            common()
+            ,resolve()
+        ]
+        ,input: './index.js'
+        ,output: {
+            name: 'stagnant'
+            ,format: 'umd'
+            ,file: './dist/stagnant.umd.js'
+            ,sourcemap: true
+        }
+    }
+    ,{
+        plugins: [
+            common()
+            ,resolve()
+            ,terser()
+        ]
+        ,input: './index.js'
+        ,output: {
+            name: 'stagnant'
+            ,format: 'umd'
+            ,file: './dist/stagnant.umd.min.js'
+            ,sourcemap: true
+        }
+    }
+]
