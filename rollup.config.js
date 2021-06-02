e1314ac947657d7eb4d555fbe0b28bafb5de490b
James Forbes <james.a.forbes@gmail.com>
2021-06-02 10:08:59 +1000

    Use honeycomb from node or the browser

--- rollup.config.js
+++ rollup.config.js
@@ -34,11 +34,29 @@ export default [
         plugins: [
             common()
             ,resolve()
+            ,terser()
+        ]
+        ,external: [
+            'node-fetch'
+        ]
+        ,input: './index.js'
+        ,output: {
+            name: 'stagnant'
+            ,format: 'umd'
+            ,file: './dist/stagnant-honey.browser.min.js'
+            ,sourcemap: true
+        }
+    }
+    ,{
+        plugins: [
+            common()
+            ,resolve()
         ]
         ,input: './honeycomb.js'
         ,output: {
             format: 'cjs'
             ,file: './honeycomb.cjs'
+            ,exports: 'default'
         }
     }
     ,{
@@ -50,6 +68,7 @@ export default [
         ,output: {
             format: 'cjs'
             ,file: './index.cjs'
+            ,exports: 'default'
         }
     }
 ]
