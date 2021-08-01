44a718b67eff30e6d7b26231c6bece1bc141ad98
James Forbes <james.a.forbes@gmail.com>
2021-08-01 12:29:28 +1000

    Prevent clobbering onevent

--- honeycomb.mjs
+++ honeycomb.mjs
@@ -11,50 +11,49 @@ export default function Main({
     , config={}
 }={}){
 
-    config = { ...stagnant.defaultConfig(), ...config }
+    async function onevent(event){
+        const name = event.parentId ? event.name : rootName
+        const body = JSON.stringify({
+            ...event.data
+            , ...data
+            , name
+            , error: event.error ? event.error.message : undefined
+            ,'error.stack': event.error ? event.error.stack : undefined
+            , 'trace.trace_id': 'trace-'+ event.traceId
+            , 'trace.span_id': 'span-' + event.id
+            , 'trace.parent_id': event.parentId ? 'span-' + event.parentId : parentId
+            , service_name: 'stagnant'
+            , duration_ms: event.endTime - event.startTime
+        })
+
+        try {
+
+            // console.log(name, event.error, event.endTime - event.startTime)
+            const response = await config.fetch(`https://api.honeycomb.io/1/events/${dataset}`, {
+                method: 'post'
+                ,headers: {
+                    'X-Honeycomb-Team': writeKey
+                    ,'X-Honeycomb-Event-Time': event.startTime
+                    ,'Content-Type': 'application/json'
+                }
+                ,body
+            })
+            config.console.log(name, response.status, body);
+            if( !event.parentId ) {
+                config.console.log('flushed', response.status )
+            }
+        } catch (e) {
+            config.console.error(e)
+        }
+
+    }
+
+    config = { ...stagnant.defaultConfig(), onevent, ...config }
     
     // Traces aren't nested, but a trace can contain nested spans.
     // We create a root trace for every invocation of Honey
     // every other event subsequently is a span within that root trace
-    const root = stagnant({
-        async onevent(event){
-            const name = event.parentId ? event.name : rootName
-            const body = JSON.stringify({
-                ...event.data
-                , ...data
-                , name
-                , error: event.error ? event.error.message : undefined
-                ,'error.stack': event.error ? event.error.stack : undefined
-                , 'trace.trace_id': 'trace-'+ event.traceId
-                , 'trace.span_id': 'span-' + event.id
-                , 'trace.parent_id': event.parentId ? 'span-' + event.parentId : parentId
-                , service_name: 'stagnant'
-                , duration_ms: event.endTime - event.startTime
-            })
-
-            try {
-
-                // console.log(name, event.error, event.endTime - event.startTime)
-                const response = await config.fetch(`https://api.honeycomb.io/1/events/${dataset}`, {
-                    method: 'post'
-                    ,headers: {
-                        'X-Honeycomb-Team': writeKey
-                        ,'X-Honeycomb-Event-Time': event.startTime
-                        ,'Content-Type': 'application/json'
-                    }
-                    ,body
-                })
-                config.console.log(name, response.status, body);
-                if( !event.parentId ) {
-                    config.console.log('flushed', response.status )
-                }
-            } catch (e) {
-                config.console.error(e)
-            }
-
-        }
-        , ...config
-    })
+    const root = stagnant(config)
 
     return root
 }
